173784c96340fd8d13c386c2d07f23afbd2a8029
Yegor Yershov <yegor6741@gmail.com>
2025-02-05 17:54:36 +0300

    mini 2

--- mini_2.cpp
+++ mini_2.cpp
@@ -10,19 +10,19 @@ using namespace std;
 
 
 /*
-Анализ сложности алгоритма деления (N, M - количество цифр в делимом и делителе соотв.):
-Текущая реализация с использованием контейнера std::vector в процессе деления
-делает операции удаления первого элемента с начала, что имеет линейную сложность,
-однако, если принебречь этим, и положить, что удаление самой "правой" цифры числа
-происходит за константу, то алгоритм делает N сдвигов, на каждом из
-которых происходит вычитание, работающее за (M + N), поэтому в среднем и худшем
-случаях алгоритм работает за O(N*(N + M)), а в лучшем случае
-(числа равны или делитель больше делимого) за O(1)
+Длинное умножение реализовано с помощью Алгоритма Карацубы
 
 Вместо std::vector можно было бы использовать std::list (двусвязный список),
 что непосредственно предоставило бы возможность за константу удалять (добавлять)
 элементы с начала.
 */
+
+
+void print_vector_rev(vector<auto> &a) {
+	for (ssize_t i = a.size() - 1; i >= 0; i--)
+		cout << a[i];
+	cout << '\n';
+}
 
 
 class uintbig_t {
@@ -31,11 +31,13 @@ private:
 	int32_t BASE = 10;
 
 	void recalc_msd() {
-		for (ssize_t i = digits.size() - 1; i >= 0; i--) {
+		msd = 0;
+		for (ssize_t i = digits.size() - 1; i > 0; i--) {
 			if (digits[i] != 0) {
 				msd = i;
 				return;
 			}
+			// digits.pop_back();
 		}
 	}
 
@@ -48,14 +50,9 @@ public:
 	}
 	uintbig_t(string n) {
 		digits = vector<int32_t>(n.size(), 0);
-		bool flag = true;
-		for (int i = 0; i < n.size(); i++) {
-			if (flag && n[i] != '0') {
-				msd = n.size() - i - 1;
-				flag = false;
-			}
+		for (int i = 0; i < n.size(); i++)
 			digits[n.size() - i - 1] = n[i] - '0';
-		}
+		recalc_msd();
 	}
 	uintbig_t(int a) {
 		digits = vector<int32_t>(1, a % 10);
@@ -66,6 +63,10 @@ public:
 			a /= 10;
 		}
 	}
+	uintbig_t(vector<int32_t> d) {
+		digits = d;
+		recalc_msd();
+	}
 	bool operator==(const uintbig_t &n) const {
 		if (msd != n.msd)
 			return false;
@@ -75,6 +76,9 @@ public:
 				return false;
 
 		return true;
+	}
+	bool operator!=(const uintbig_t &n) const {
+		return !(*this == n);
 	}
 	bool operator>=(const uintbig_t &n) const {
 		if (msd > n.msd)
@@ -164,7 +168,16 @@ public:
 		recalc_msd();
 		return *this;
 	}
-
+	uintbig_t operator+(const uintbig_t& other) const {
+		uintbig_t n = *this;
+		n += other;
+		return n;
+	}
+	uintbig_t operator-(const uintbig_t& other) const {
+		uintbig_t n = *this;
+		n -= other;
+		return n;
+	}
 	uintbig_t &operator/=(const uintbig_t &other) {
 		if (other == 0 || *this == 0) {
 			*this = 0;
@@ -193,8 +206,6 @@ public:
 			}
 			n.base_shr();
 			new_digits.push_back(l_res);
-			if (n == 0)
-				break;
 		}
 
 		reverse(new_digits.begin(), new_digits.end());
@@ -202,6 +213,88 @@ public:
 
 		recalc_msd();
 		return *this;
+	}
+	uintbig_t &operator*=(const uintbig_t &other) {
+		if (*this == 0 || other == 0) {
+			*this = 0;
+			return *this;
+		}
+
+		uintbig_t first, second;
+		if (digits.size() >= other.digits.size()) {
+			first = *this;
+			second = other;
+		} else {
+			first = other;
+			second = *this;
+		}
+
+		while (first.digits.size() > second.digits.size())
+			second.digits.push_back(0);
+
+		if (first.digits.size() & 1) {
+			first.digits.push_back(0);
+			second.digits.push_back(0);
+		}
+
+		if (second.msd == 0) {
+			*this = 0;
+			for (int32_t i = 0; i < second.digits[0]; i++)
+				*this += first;
+			recalc_msd();
+			return *this;
+		}
+
+		if (first.msd == 0) {
+			*this = 0;
+			for (int32_t i = 0; i < first.digits[0]; i++)
+				*this += second;
+			recalc_msd();
+			return *this;
+		}
+
+		/*
+		a      b
+		 12 34
+		 56 78 станислав валерьевич
+		c     d
+		*/
+
+		vector<int32_t> a_digits, b_digits, c_digits, d_digits;
+		for (ssize_t i = 0; i < first.digits.size(); i++) {
+			if (i < (first.digits.size() >> 1)) {
+				b_digits.push_back(first.digits[i]);
+				d_digits.push_back(second.digits[i]);
+			} else {
+				a_digits.push_back(first.digits[i]);
+				c_digits.push_back(second.digits[i]);
+			}
+		}
+
+		uintbig_t a(a_digits), b(b_digits), c(c_digits), d(d_digits);
+		uintbig_t ac = a;
+		ac *= c;
+		uintbig_t bd = b;
+		bd *= d;
+
+		uintbig_t eq = a + b;
+		eq *= (c + d);
+		eq -= bd;
+		eq -= ac;
+
+		for (size_t i = 0; i < first.digits.size(); i++) {
+			ac.base_shl();
+			if (i < (first.digits.size() >> 1))
+				eq.base_shl();
+		}
+
+		*this = ((ac + bd) + eq);
+		return *this;
+	}
+	uintbig_t operator*(const uintbig_t& other) const {
+		uintbig_t n = *this;
+		n *= other;
+		return n;
 	}
 };
 
@@ -233,10 +326,18 @@ int main(void)
 {
 	// list<int> q = {1, 8, 13};
 	// cout << *(q.begin()) << '\n';
+	uintbig_t test1_a = 13424;
+	uintbig_t test1_b = 77777;
+	cout << test1_a << " * " << test1_b << " = " << (test1_a * test1_b) << endl;
 
+	uintbig_t test2_a = 1234;
+	uintbig_t test2_b = 5678;
+	cout << test2_a << " * " << test2_b << " = " << (test2_a * test2_b) << endl;
+
+	cout << "Now enter 2 numbers manually:\n";
 	uintbig_t a, b;
 	cin >> a >> b;
-	cout << (a /= b) << '\n';
+	cout << (a *= b) << '\n';
 
 	return 0;
 }
