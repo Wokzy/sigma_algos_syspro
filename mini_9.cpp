a38797242766be4f0537caa1c839b0cd935120d2
Yegor Yershov <yegor6741@gmail.com>
2025-02-15 13:48:45 +0700

    mini 9

--- mini_9.cpp
+++ mini_9.cpp
@@ -16,25 +16,46 @@ void shell_sort(vector<string>& data) {
 	}
 }
 
+
+void count_sort(vector<string> &arr, int index) {
+	int n = arr.size();
+	vector<string> res(n, "");
+	vector<int> count(256, 0);
+
+	for (int i = 0; i < n; i++) {
+		if (index >= arr[i].size())
+			count[0]++;
+		else
+			count[arr[i][index]]++;
+	}
+
+	for (int i = 1; i < 256; i++)
+		count[i] += count[i - 1];
+
+	for (int i = n - 1; i >= 0; i--) {
+		int k = 0;
+		if (arr[i].size() > index)
+			k = arr[i][index];
+		res[count[k] - 1] = arr[i];
+		count[k]--;
+	}
+
+	for (int i = 0; i < n; i++)
+		arr[i] = res[i];
+}
+
+
 void lsd_radix_sort(vector<string> &arr) {
 	if (arr.size() <= 1)
 		return;
-	int k = arr[0].size();
 
-	for (int i = 0; i < k; i++) {
-		for (int j = 1; j < arr.size(); j++) {
-			if (arr[j].size() > k)
-				k = arr[j].size();
-			for (int l = j; l > 0; l--) {
-				if (arr[l].size() < arr[l - 1].size())
-					swap(arr[l], arr[l - 1]);
-				else if ((arr[l].size() > i) && (arr[l][i] < arr[l - 1][i]))
-					swap(arr[l], arr[l - 1]);
-				else
-					break;
-			}
-		}
-	}
+	int k = 0;
+	for (auto &c: arr)
+		if (c.size() > k)
+			k = c.size();
+
+	for (int i = 0; i < k; i++)
+		count_sort(arr, i);
 }
 
 
@@ -47,7 +68,7 @@ signed main(void) {
 	for (auto& c: test2)
 		cout << c << '\n';
 
-	cout << "\n LSD-radix sort\n";
+	cout << "\nLSD-radix sort\n";
 	for (auto& c: test1)
 		cout << c << '\n';
 
