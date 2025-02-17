45ddc06a74b6a7c30d56e3a595e15532cf98143c
Yegor Yershov <yegor6741@gmail.com>
2025-02-18 00:09:59 +0700

    mini 11 addon

--- mini_11.cpp
+++ mini_11.cpp
@@ -3,7 +3,27 @@
 
 using namespace std;
 
-void quick_sort(vector<int> &arr, int l, int r) {
+template<typename T>
+void print_arr(vector<T> arr) {
+	if (arr.size() == 0)
+		return;
+	cout << arr[0];
+	for (int i = 1; i < arr.size(); i++)
+		cout << ' ' << arr[i];
+	cout << '\n';
+}
+
+template<typename T>
+void print_arr(vector<T> arr, int l, int r) {
+	if (arr.size() == 0)
+		return;
+	cout << arr[l];
+	for (int i = 1; i < r - l; i++)
+		cout << ' ' << arr[l + i];
+	cout << '\n';
+}
+
+void quick_sort_simple(vector<int> &arr, int l, int r) {
 	if (r - l <= 1)
 		return;
 
@@ -11,31 +31,62 @@ void quick_sort(vector<int> &arr, int l, int r) {
 	int p = abs(arr[0]) % n; // как мне кажется - достаточно случайный элемент
 	swap(arr[l], arr[l + p]);
 
-	int i = 1, j = 1;
-	while (j < n) {
-		if (arr[j + l] <= arr[l]) {
-			if (i != j)
-				swap(arr[i + l], arr[j + l]);
-			i++;
-		}
-		j++;
-	}
-	if (i != 1)
-		swap(arr[l], arr[i + l - 1]);
+	int i = -1, k = 0;
 
-	quick_sort(arr, l, i + l - 1);
-	quick_sort(arr, i + l, r);
+	for (int j = 1; j < n; j++) {
+		if (arr[l + j] < arr[l + k]) {
+			swap(arr[l + i + 1], arr[l + j]);
+			swap(arr[l + k + 1], arr[l + j]);
+			i++;
+			k++;
+		} else if (arr[l + j] == arr[l + k]) {
+			swap(arr[l + k + 1], arr[l + j]);
+			k++;
+		}
+	}
+
+	quick_sort_simple(arr, l, i + l + 1);
+	quick_sort_simple(arr, k + l + 1, r);
+}
+
+void quick_sort_advanced(vector<int> &arr, int l, int r) {
+	if (r <= l)
+		return;
+
+	int v = arr[(l + r) / 2];
+	int i = l;
+	int j = r;
+
+	while (i <= j) {
+		while (arr[i] < v)
+			i++;
+		while (arr[j] > v)
+			j--;
+		if (i >= j)
+			break;
+		swap(arr[i++], arr[j--]);
+	}
+
+	quick_sort_advanced(arr, l, j);
+	quick_sort_advanced(arr, j + 1, r);
 }
 
 
 signed main(void) {
-	// vector<int> test = {0, 0, 1, 2, 0, 2, 1, 1, 0, 0, 0, 1};
-	vector<int> test = {4, 1, 7, 8, 3, 33, -10, 2, 12};
+	// vector<int> test = {2, 5, 3, 1};
+	// vector<int> test = {2, 5, 3, 1, -10, 7, 143, 32817, -123};
+	vector<int> test1 = {4, 9, 7, 8, 8, 3, 33, -10, 8, 8, 8, 2, 12};
+	vector<int> test2 = {4, 9, 7, 8, 8, 3, 33, -10, 8, 8, 8, 2, 12};
 
-	quick_sort(test, 0, test.size());
-	cout << test[0];
-	for (int i = 1; i < test.size(); i++)
-		cout << ' ' << test[i];
-	cout << '\n';
+	cout << "Initial\n";
+	print_arr(test1);
+
+	cout << "Lomuto's\n";
+	quick_sort_simple(test1, 0, test1.size());
+	print_arr(test1);
+
+	cout << "Hoare's\n";
+	quick_sort_advanced(test2, 0, test2.size() - 1);
+	print_arr(test2);
 	return 0;
 }
