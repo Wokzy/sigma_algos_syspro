b6085ebf7b0fcd3e2557b6ab404c64e946fa2d3a
Wokzy <yegor6741@gmail.com>
2025-02-18 17:32:53 +0700

    mini 12 final solution

--- mini_12.c
+++ mini_12.c
@@ -6,9 +6,18 @@
 typedef struct
 {
 	int *lomuto;
+	int *lomuto_advanced;
 	int *hoare;
 } test_ctx;
 
+void swap(int *x, int *y) {
+	// *x ^= *y;
+	// *y ^= *x;
+	// *x ^= *y;
+	int temp = *x;
+	*x = *y;
+	*y = temp;
+}
 
 void print_arr(int *arr, size_t n) {
 	if (n == 0)
@@ -50,9 +59,7 @@ void quick_sort_hoare(int *arr, int l, int r) {
 int *lomuto_partition_branchfree(int *first, int *last) {
 	--last;
 	if (*first > *last) {
-		int temp = *first;
-		*first = *last;
-		*last = *first;
+		swap(first, last);
 	}
 	int *pivot_pos = first;
 	int pivot = *first;
@@ -82,12 +89,41 @@ void quick_sort_lomuto_advanced(int *first, int *last) {
 	quick_sort_lomuto_advanced(pivot + 1, last);
 }
 
+void quick_sort_lomuto_simple(int *arr, int l, int r) {
+	if (r - l <= 1)
+		return;
+
+	int n = r - l;
+	int p = abs(arr[0]) % n;
+	swap(&arr[l], &arr[l + p]);
+
+	int i = -1, k = 0;
+
+	for (int j = 1; j < n; j++) {
+		if (arr[l + j] < arr[l + k]) {
+			swap(&arr[l + i + 1], &arr[l + j]);
+			swap(&arr[l + k + 1], &arr[l + j]);
+			i++;
+			k++;
+		} else if (arr[l + j] == arr[l + k]) {
+			swap(&arr[l + k + 1], &arr[l + j]);
+			k++;
+		}
+	}
+
+	quick_sort_lomuto_simple(arr, l, i + l + 1);
+	quick_sort_lomuto_simple(arr, k + l + 1, r);
+}
+
+
 test_ctx gen_random_ctx(size_t size) {
 	test_ctx res;
 	res.lomuto = malloc(sizeof(int) * size);
+	res.lomuto_advanced = malloc(sizeof(int) * size);
 	res.hoare = malloc(sizeof(int) * size);
 	for (size_t i = 0; i < size; i++) {
-		res.lomuto[i] = rand() % 20;
+		res.lomuto[i] = rand();
+		res.lomuto_advanced[i] = res.lomuto[i];
 		res.hoare[i] = res.lomuto[i];
 	}
 
@@ -97,23 +133,32 @@ test_ctx gen_random_ctx(size_t size) {
 void clear_ctx(test_ctx ctx) {
 	free(ctx.lomuto);
 	free(ctx.hoare);
+	free(ctx.lomuto_advanced);
 }
 
 signed main(void) {
 	srand(time(NULL));
 
-	for (size_t n = 10; n <= 100000; n *= 10) {
+	clock_t startclock, endclock;
+
+	for (size_t n = 100000; n <= 10000000; n *= 10) {
 		printf("N = %zu\n", n);
 		test_ctx ctx = gen_random_ctx(n);
 
-		clock_t startclock = clock();
-		quick_sort_hoare(ctx.hoare, 0, n - 1);
-		clock_t endclock = clock();
-		printf("\tHoare : %9.4f\n", (float)(endclock-startclock)/(float)CLOCKS_PER_SEC);
 		startclock = clock();
-		quick_sort_lomuto_advanced(ctx.lomuto, ctx.lomuto + n);
+		quick_sort_hoare(ctx.hoare, 0, n - 1);
 		endclock = clock();
-		printf("\tLomuto : %9.4f\n", (float)(endclock-startclock)/(float)CLOCKS_PER_SEC);
+		printf("\tHoare : %9.4f\n", (float)(endclock-startclock)/(float)CLOCKS_PER_SEC);
+
+		startclock = clock();
+		quick_sort_lomuto_simple(ctx.lomuto, 0, n);
+		endclock = clock();
+		printf("\tLomuto simple (3 pointers): %9.4f\n", (float)(endclock-startclock)/(float)CLOCKS_PER_SEC);
+
+		startclock = clock();
+		quick_sort_lomuto_advanced(ctx.lomuto_advanced, ctx.lomuto_advanced + n);
+		endclock = clock();
+		printf("\tLomuto Branchfree: %9.4f\n", (float)(endclock-startclock)/(float)CLOCKS_PER_SEC);
 		clear_ctx(ctx);
 	}
 
